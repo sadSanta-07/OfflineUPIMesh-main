0a0da5cfd8850119bb2a911cf9789b3b675efdc0
pechkas <iffcogc34@gmail.com>
2026-07-25 01:53:29 +0530

    Replace shared_mutex with mutex in IdempotencyService for simplified locking

--- src/service/idempotency.cpp
+++ src/service/idempotency.cpp
@@ -1,11 +1,9 @@
 #include "upi_mesh/service/idempotency.hpp"
-#include <mutex>
 
 namespace upi::service {
 
 bool IdempotencyService::claim_hash(const std::string& hash) {
-    std::unique_lock<std::shared_mutex> lock(mutex_);
-
+    std::lock_guard<std::mutex> lock(mutex_);
 
     auto [it, inserted] = seen_hashes_.emplace(hash, std::chrono::system_clock::now());
     
@@ -13,13 +11,12 @@ bool IdempotencyService::claim_hash(const std::string& hash) {
 }
 
 size_t IdempotencyService::size() const {
-    // Acquire
-    std::shared_lock<std::shared_mutex> lock(mutex_);
+    std::lock_guard<std::mutex> lock(mutex_);
     return seen_hashes_.size();
 }
 
 void IdempotencyService::clear() {
-    std::unique_lock<std::shared_mutex> lock(mutex_);
+    std::lock_guard<std::mutex> lock(mutex_);
     seen_hashes_.clear();
 }
 
